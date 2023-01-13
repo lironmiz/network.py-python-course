62ceb00cf1b762826fcbbf0683f5bfff513e0f3f
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-13 07:45:49 +0200

    Create unit2_ex2.2.4.py

---
+++ unit2_ex2.2.4.py
@@ -0,0 +1,8 @@
+# exercise 2.2.4 from unit 2
+'''
+We are surfing on our laptop in the Chrome browser, and now we would like to watch a video from YouTube. What are the IP and port numbers to which
+the browser should go in order to browse the YouTube site?
+'''
+
+# Answer: The IP address is 216.58.207.46 and the port number is 80
+
