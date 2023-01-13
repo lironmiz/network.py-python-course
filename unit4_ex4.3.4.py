5edc1cec0d26e6b1f87ae431b19e24ab0f31edd6
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-13 15:55:22 +0200

    Create unit4_ex4.3.4.py

---
+++ unit4_ex4.3.4.py
@@ -0,0 +1,17 @@
+# exercise 4.3.4 from unit 4
+'''
+Here are three sentences. For each of the sentences, determine whether it is true or false.
+The sentences are based on the program code that appears in unit 4.3.
+
+'''
+
+# Answers: 
+
+# 1: When the program starts running, the list client_sockets includes only one member - wrong
+
+# 2: If the ready_to_read list includes a request from an existing client's socket, the server
+# should open a connection to it using the accept method - wrong
+
+# 3: It is not possible to run more than one client on the same computer - wrong
+
+# 4: The call to the select function in the code is carried out as long as the server is active - true
