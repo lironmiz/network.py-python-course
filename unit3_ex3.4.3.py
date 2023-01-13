72c2df72b4a46f8018244ad8f96576ddad1f4b3e
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-13 09:30:01 +0200

    Create unit3_ex3.4.3.py

---
+++ unit3_ex3.4.3.py
@@ -0,0 +1,31 @@
+# exercise 3.4.3 from unit 3
+'''
+Here is a list of actions. For each operation, choose where it is performed - UDP server
+or UDP client. Please note, there are 
+actions that are performed both on the server and on the client, there are actions
+that are performed neither on the server nor on the client.
+
+'''
+
+# Answers: 
+'''
+1: bind -> udp server
+
+2: listen -> none of them 
+
+3: socket.socket(socket.AF_INET, socket.SOCK_DGRAM) -> both of them 
+
+4: accept -> none of them
+
+5: connect -> none of them 
+
+6: send -> none of them 
+
+7: recv -> none of them 
+
+8: recvfrom -> both of them 
+
+9: sendto -> both of them 
+
+10: close -> both of them 
+'''
