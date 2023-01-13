e67b73f9c328ea4759b2cdac205d8a20b41ab357
Liron-Mizrahi <91504420+lironmiz@users.noreply.github.com>
2023-01-13 07:35:49 +0200

    Create unit1_ex1.4.1.py

---
+++ unit1_ex1.4.1.py
@@ -0,0 +1,16 @@
+# exercise 1.4.1 from unit 1
+'''
+Here is a list of tasks for a reliable protocol in the traffic layer.
+Mark the actions performed on the sender's side:
+'''
+
+# Answer:
+
+# 1: to add number of each message
+
+# 2: Send the message to the destination
+
+# 3: Wait for confirmation from the receiving party that the message has been received
+
+# 4: Send the following message
+
